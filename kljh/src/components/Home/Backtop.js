86cd2db06cfe39329d61da1fa3115649a158226a
weihuade <758785052@qq.com>
2019-02-11 10:27:01 +0800

    做了回到顶部

---
+++ kljh/src/components/Home/Backtop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BackTop } from 'antd';
+class Backtop extends React.Component{
+	 render(){
+        return (
+            <div>
+			<BackTop style={{ bottom: 55,right:15 ,width:30,height:30}}>
+    		</BackTop>
+            </div>
+        )
+    }
+}
+
+export default Backtop;
--- kljh/src/components/Home/index.js
+++ kljh/src/components/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Homebody from './Homebody';
+import Backtop from './Backtop';
 class Home extends React.Component{
 	 render(){
         return (
             <div>
              <Header></Header>
              <Homebody></Homebody>
+             <Backtop></Backtop>
             </div>
         )
     }
