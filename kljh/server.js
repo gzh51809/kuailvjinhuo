704f001fc75b0fd72eb317e61d77980c1db45bc6
weihuade <758785052@qq.com>
2019-02-12 14:31:54 +0800

    分类样式完成

--- kljh/server.js
+++ kljh/server.js
@@ -8,6 +8,9 @@ app.get("/",(req,res)=>{
   	let data=req.query.rq;
   	let fenlei=req.query.fenlei
 	let tuijian=req.query.tuijian
+	let toubu=req.query.toubu
+	let lei=req.query.lei
+	let con=req.query.con
     res.append("Access-Control-Allow-Origin","*")
     if(data=="http://www.kuailvzaixian.com/wxmall/api/promotion/banner/list?cityId=440600&channel=1&_=1549120491634"){
     	request.get("http://www.kuailvzaixian.com/wxmall/api/promotion/banner/list?cityId=440600&channel=1&_=1549120491634",(err,response,body)=>{
@@ -22,6 +25,24 @@ app.get("/",(req,res)=>{
  
  	if(tuijian=="http://www.kuailvzaixian.com/wxmall/api/goods/recommendation/list?cityId=440600&pageNo=1&pageSize=20&_=1549469017061"){
  		request.get("http://www.kuailvzaixian.com/wxmall/api/goods/recommendation/list?cityId=440600&pageNo=1&pageSize=20&_=1549469017061",(err,response,body)=>{
+			res.send(body)
+		})
+ 	}
+ 	
+ 	if(toubu=="http://www.kuailvzaixian.com/wxmall/api/goods/category/first/list?cityId=440600&_=1549936055712"){
+ 		request.get("http://www.kuailvzaixian.com/wxmall/api/goods/category/first/list?cityId=440600&_=1549936055712",(err,response,body)=>{
+			res.send(body)
+		})
+ 	}
+ 	
+ 	if(lei=="http://www.kuailvzaixian.com/wxmall/api/goods/category/second/list?cityId=440600&cat1Id=1001108&_=1549936055853"){
+ 		request.get("http://www.kuailvzaixian.com/wxmall/api/goods/category/second/list?cityId=440600&cat1Id=1001108&_=1549936055853",(err,response,body)=>{
+			res.send(body)
+		})
+ 	}
+ 	
+ 	if(con=="http://www.kuailvzaixian.com/wxmall/api/goods/list?cat1Id=1001108&cat2Id=-2000&pageNo=1&pageSize=20&cityId=440600&_=1549942010713"){
+ 		request.get("http://www.kuailvzaixian.com/wxmall/api/goods/list?cat1Id=1001108&cat2Id=-2000&pageNo=1&pageSize=20&cityId=440600&_=1549942010713",(err,response,body)=>{
 			res.send(body)
 		})
  	}
--- kljh/src/components/Classify/Classify.js
+++ kljh/src/components/Classify/Classify.js
@@ -1,17 +1,148 @@
-import React,{ Component } from 'react';
+import React, {
+	Component
+} from 'react';
+import { NavLink } from 'react-router-dom';
+import axios from 'axios'
+import '../../sass/classify/classify.scss'
+class Classify extends Component {
 
-class Classify extends Component{
-	
-	constructor(){
+	constructor() {
 		super();
-		this.state={
-			
+		this.state = {
+			classify: [],
+			leibie: [],
+			content:[]
 		}
 	}
-	
-	render(){
+
+	componentDidMount() {
+		axios.get("http://localhost:3334/", {
+			params: {
+				toubu: "http://www.kuailvzaixian.com/wxmall/api/goods/category/first/list?cityId=440600&_=1549936055712",
+			}
+		}).then(res => {
+			let data1 = res.data.data.categoryList
+//			console.log(res.data.data.categoryList)
+			this.setState({
+				classify: data1,
+			})
+		})
+
+		axios.get("http://localhost:3334/", {
+			params: {
+				lei: "http://www.kuailvzaixian.com/wxmall/api/goods/category/second/list?cityId=440600&cat1Id=1001108&_=1549936055853",
+			}
+		}).then(res => {
+			let data2 = res.data.data.categoryList
+//			console.log(res.data.data.categoryList)
+			this.setState({
+				leibie: data2,
+			})
+		})
+		
+		axios.get("http://localhost:3334/", {
+			params: {
+				con: "http://www.kuailvzaixian.com/wxmall/api/goods/list?cat1Id=1001108&cat2Id=-2000&pageNo=1&pageSize=20&cityId=440600&_=1549942010713",
+			}
+		}).then(res => {
+			let data3 = res.data.data.goodsList
+			console.log(res.data.data.goodsList)
+			this.setState({
+				content: data3,
+			})
+		})
+		
+	}
+
+	render() {
 		return(
-			<div>Classify</div>
+			<div className="classify">
+			<div className="category-page">
+				<NavLink to="search">
+				<div className="search-part">
+					<div className="searchBar-part">
+						<div className="search">
+							<img alt="" className="icon-search" src={require('../../img/homeimg/search.png')} />
+							<span className="search-placeholder">请输入商品名称</span>
+						</div>
+						</div>
+					</div>
+					</NavLink>
+					<section className="category-level1">
+						<div className="nav-list">
+						{
+							this.state.classify.map((item,idx)=>(
+							<div className="nav-item" key={idx}>
+								<div className="cate1-img">
+								<img alt="" src={item.iconUrl} />
+								</div>
+								<div className="cate-name">{item.name}</div>
+							</div>
+							))
+						}	
+						</div>
+					</section>
+					
+					<section className="Category-level2">
+						<div className="nav2-list">
+						{
+							this.state.leibie.map((item,idx)=>(
+						<div className="nav2-item" key={idx}>
+							<img alt=""  src={item.iconUrl} />
+							<span>{item.name}</span>
+							</div>
+							))
+						}
+						</div>
+					</section>
+					<div className="tags-part"><div className="cate-second-name">爆品</div></div>
+					
+					<section className="csulist-main-content">
+						<div className="csu-content">
+							{
+								this.state.content.map((item,idx)=>(
+							<div key={idx}>
+								<div className="csu-card-container">
+									<div className="csu-card-content">
+										
+										<div className="image-box-container">
+											<div className="image-box">
+											<img alt="" src={item.spuResVo.picUrls} />
+											</div>
+										</div>
+									<div className="csu-info">
+												<div className="csu-name"> {item.spuResVo.spuTitle} </div>
+									         <div className="csu-other-tag-group"></div>
+									       		<div className="csu-info-bottom">
+									       		<div className="csu-unit-desc">{item.spuResVo.skuDescCombination}</div>
+									       		<div className="csu-price-container">
+									       		<div className="csu-price">
+										       		<div className="visible-for-login">
+													  登录可见价格
+													</div>
+												</div>
+												<div className="csu-button-container">
+													<div className="csu-item-touch">
+														<div className="csu-item-btn">
+														<img alt="" src={require('../../img/homeimg/jiahao.png')} />
+														</div>
+													</div>
+												</div>
+												
+									       		</div>
+									       		</div>
+										</div>
+										
+									</div>
+								</div>
+							</div>
+							)) 
+						}
+						</div>
+					</section>
+					
+				</div>
+			</div>
 		)
 	}
 }
